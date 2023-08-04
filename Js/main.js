cde99cf115806d861ed82544406f8f6877c5653b
Asiel Jiménez <121409312+ingasiel@users.noreply.github.com>
2023-08-04 17:10:32 -0600

    Add files via upload

---
+++ Js/main.js
@@ -0,0 +1,100 @@
+function vinculo1(){
+    location.href="https://xandacompany.github.io/";
+}
+
+function celular(){
+    document.getElementById("seleccion1").style.background="linear-gradient(45deg,#046ade,#a802fa)";
+    document.getElementById("seleccion2").style.background="none";
+    document.getElementById("seleccion3").style.background="none";
+
+
+
+    document.getElementById("efecto2_aparecer1").style.display="flex";
+    document.getElementById("efecto2_aparecer2").style.display="flex";
+    document.getElementById("efecto2_aparecer3").style.display="flex";
+    document.getElementById("efecto2_aparecer4").style.display="flex";
+    document.getElementById("efecto2_aparecer5").style.display="flex";
+    document.getElementById("efecto2_aparecer6").style.display="flex";
+
+    document.getElementById("efecto_aparecer1").style.display="none";
+    document.getElementById("efecto_aparecer2").style.display="none";
+    document.getElementById("efecto_aparecer3").style.display="none";
+    document.getElementById("efecto_aparecer4").style.display="none";
+    document.getElementById("efecto_aparecer5").style.display="none";
+    document.getElementById("efecto_aparecer6").style.display="none";
+
+    document.getElementById("accion_de_tarjeta1").style.display="none";
+    document.getElementById("accion_de_tarjeta2").style.display="none";
+    document.getElementById("accion_de_tarjeta3").style.display="none";
+    document.getElementById("accion_de_tarjeta4").style.display="none";
+    document.getElementById("accion_de_tarjeta5").style.display="none";
+    document.getElementById("accion_de_tarjeta6").style.display="none";
+}
+
+function web(){
+    document.getElementById("seleccion2").style.background="linear-gradient(45deg,#046ade,#a802fa)";
+    document.getElementById("seleccion1").style.background="none";
+    document.getElementById("seleccion3").style.background="none";
+
+
+
+    document.getElementById("accion_de_tarjeta1").style.display="flex";
+    document.getElementById("accion_de_tarjeta2").style.display="flex";
+    document.getElementById("accion_de_tarjeta3").style.display="flex";
+    document.getElementById("accion_de_tarjeta4").style.display="flex";
+    document.getElementById("accion_de_tarjeta5").style.display="flex";
+    document.getElementById("accion_de_tarjeta6").style.display="flex";
+
+    document.getElementById("efecto_aparecer1").style.display="none";
+    document.getElementById("efecto_aparecer2").style.display="none";
+    document.getElementById("efecto_aparecer3").style.display="none";
+    document.getElementById("efecto_aparecer4").style.display="none";
+    document.getElementById("efecto_aparecer5").style.display="none";
+    document.getElementById("efecto_aparecer6").style.display="none";
+
+    document.getElementById("efecto2_aparecer1").style.display="none";
+    document.getElementById("efecto2_aparecer2").style.display="none";
+    document.getElementById("efecto2_aparecer3").style.display="none";
+    document.getElementById("efecto2_aparecer4").style.display="none";
+    document.getElementById("efecto2_aparecer5").style.display="none";
+    document.getElementById("efecto2_aparecer6").style.display="none";
+}
+
+function pc(){
+    document.getElementById("seleccion3").style.background="linear-gradient(45deg,#046ade,#a802fa)";
+    document.getElementById("seleccion2").style.background="none";
+    document.getElementById("seleccion1").style.background="none";
+
+
+
+    document.getElementById("accion_de_tarjeta1").style.display="none";
+    document.getElementById("accion_de_tarjeta2").style.display="none";
+    document.getElementById("accion_de_tarjeta3").style.display="none";
+    document.getElementById("accion_de_tarjeta4").style.display="none";
+    document.getElementById("accion_de_tarjeta5").style.display="none";
+    document.getElementById("accion_de_tarjeta6").style.display="none";
+
+    document.getElementById("efecto_aparecer1").style.display="flex";
+    document.getElementById("efecto_aparecer2").style.display="flex";
+    document.getElementById("efecto_aparecer3").style.display="flex";
+    document.getElementById("efecto_aparecer4").style.display="flex";
+    document.getElementById("efecto_aparecer5").style.display="flex";
+    document.getElementById("efecto_aparecer6").style.display="flex";
+
+    document.getElementById("efecto2_aparecer1").style.display="none";
+    document.getElementById("efecto2_aparecer2").style.display="none";
+    document.getElementById("efecto2_aparecer3").style.display="none";
+    document.getElementById("efecto2_aparecer4").style.display="none";
+    document.getElementById("efecto2_aparecer5").style.display="none";
+    document.getElementById("efecto2_aparecer6").style.display="none";
+}
+
+function cargar_pdf(){
+    document.getElementById("cargar_pdf_2").style.display="flex";
+    document.getElementById("ocultar_para_pdf").style.display="none";
+    document.getElementById("ocultar_para_pdf_2").style.display="none";
+    document.getElementById("ocultar_para_pdf_3").style.display="none";
+
+    return cargar_pdf();
+
+}
